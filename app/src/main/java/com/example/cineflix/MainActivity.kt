bb960114bb2a3bd17aa78c810770f8bd9ac5582e
SammuDev <samucas878@gmail.com>
2023-07-09 10:32:49 -0300

    feat: Separando a 'recyclerview' da 'MainActivity'

--- app/src/main/java/com/example/cineflix/MainActivity.kt
+++ app/src/main/java/com/example/cineflix/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.cineflix
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = MainAdapter()
+        recyclerView = findViewById(R.id.recyclerview_main)
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
