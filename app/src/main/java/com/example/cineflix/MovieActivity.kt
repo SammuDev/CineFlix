dffb6526a9aa89edfbe9b2d0d5d8445c564966f6
SammuDev <samucas878@gmail.com>
2023-07-11 08:02:05 -0300

    feat: Criando uma 'toolbar' personalizada e a adicionando na atividade 'MovieActivity'

--- app/src/main/java/com/example/cineflix/MovieActivity.kt
+++ app/src/main/java/com/example/cineflix/MovieActivity.kt
@@ -2,10 +2,17 @@ package com.example.cineflix
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar_movie)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.rollback_arrow)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
