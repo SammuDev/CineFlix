4c8069711dc9984752f880fda5a7501a3f6e3f10
SammuDev <samucas878@gmail.com>
2023-07-12 03:44:39 -0300

    feat: Personalizando o cover de apresentação

--- app/src/main/java/com/example/cineflix/MovieActivity.kt
+++ app/src/main/java/com/example/cineflix/MovieActivity.kt
@@ -1,8 +1,11 @@
 package com.example.cineflix
 
+import android.graphics.drawable.LayerDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 
 class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +17,13 @@ class MovieActivity : AppCompatActivity() {
 
         supportActionBar?.setHomeAsUpIndicator(R.drawable.rollback_arrow)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val layerDrawable: LayerDrawable =
+            ContextCompat.getDrawable(this, R.drawable.shadows) as LayerDrawable
+        val imageDrawable = ContextCompat.getDrawable(this, R.drawable.golden_soldiers)
+        layerDrawable.setDrawableByLayerId(R.id.cover_drawable, imageDrawable)
+
+        val imageCover: ImageView = findViewById(R.id.image_movie)
+        imageCover.setImageDrawable(layerDrawable)
     }
 }
